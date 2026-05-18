d60c4db3c6e94c64566614d4cb333e41a53084e3
Guilherme <lerichlesss@gmail.com>
2026-05-18 14:43:54 -0300

    landingpage

--- src/index.ts
+++ src/index.ts
@@ -7,16 +7,17 @@ import metricasRoutes from "./routes/metricasRoutes";
 import path from "path";
 dotenv.config();
 
-
 const app = express();
 app.use(cors());
 app.use(express.static(path.join(__dirname, "../public")));
 app.use(express.json());
 
 app.get("/ping", (req, res) => res.json({ status: "online" }));
+app.get("/", (req, res) => res.sendFile("landing.html", { root: "public" }));
 app.use("/auth", authRoutes);
 app.use("/upload", uploadRoutes);
 app.use("/metricas", metricasRoutes);
+
 const PORTA = process.env.PORTA || 3000;
 app.listen(PORTA, () => console.log(`WhisperOps rodando na porta ${PORTA}`));
 
